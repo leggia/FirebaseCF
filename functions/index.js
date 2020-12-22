0abc2b881caa064bbacf7833dd8a7edf3724328b
leggia <luisomar07@gmail.com>
2020-12-22 06:05:19 -0400

    probando firebase

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,16 @@ admin.initializeApp({
     credential: admin.credential.cert('./permiso.json'),
     databaseUrl:""
 })
+const db=admin.firestore()
+
 app.get('/hello-world', (req, res) => {
     return res.status(200).json({message: 'hola Mundo'})
 })
+app.post('/api/productos', async (req, res)=>{
+    await db.collection('productos1').doc("/"+ req.body.id+"/")
+    .create({name: req.body.name})
+    return res.status(204).json();
+})
+
 exports.app=functions.https.onRequest(app);
 
